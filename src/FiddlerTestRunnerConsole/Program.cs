966772a2f41f1e36f30e48cc3b421afbaf41050b
JoeHosman <joe.hosman@gmail.com>
2013-01-02 21:18:44 -0600

    capturing https requests successful

--- src/FiddlerTestRunnerConsole/Program.cs
+++ src/FiddlerTestRunnerConsole/Program.cs
@@ -13,6 +13,9 @@ namespace FiddlerTestRunnerConsole
     {
         private static readonly ILog Log = LogManager.GetCurrentClassLogger();
         private static bool _needMoreInput;
+        private static Proxy _oSecureEndpoint;
+        static string sSecureEndpointHostname = "localhost";
+        static int iSecureEndpointPort = 7777;
 
         static void Main(string[] args)
         {
@@ -45,6 +48,50 @@ namespace FiddlerTestRunnerConsole
                     Log.Info(m => m("AfterSessionComplete: {0}", oS.url));
                 };
 
+            // For the purposes of this demo, we'll forbid connections to HTTPS 
+            // sites that use invalid certificates. Change this from the default only
+            // if you know EXACTLY what that implies.
+            Fiddler.CONFIG.IgnoreServerCertErrors = false;
+
+            // ... but you can allow a specific (even invalid) certificate by implementing and assigning a callback...
+            // FiddlerApplication.OnValidateServerCertificate += new System.EventHandler<ValidateServerCertificateEventArgs>(CheckCert);
+
+            FiddlerApplication.Prefs.SetBoolPref("fiddler.network.streaming.abortifclientaborts", true);
+
+            // For forward-compatibility with updated FiddlerCore libraries, it is strongly recommended that you 
+            // start with the DEFAULT options and manually disable specific unwanted options.
+            FiddlerCoreStartupFlags oFCSF = FiddlerCoreStartupFlags.Default;
+
+            // E.g. If you want to add a flag, start with the Defaults and "or" it in:
+            // oFCSF = (oFCSF | FiddlerCoreStartupFlags.CaptureFTP);
+            oFCSF = (oFCSF | FiddlerCoreStartupFlags.DecryptSSL);
+
+            // ... or if you don't want a flag in the defaults, "and not" it out:
+            // Uncomment the next line if you don't want FiddlerCore to act as the system proxy
+            oFCSF = (oFCSF & ~FiddlerCoreStartupFlags.RegisterAsSystemProxy);
+            oFCSF = (oFCSF & ~FiddlerCoreStartupFlags.MonitorAllConnections);
+            // or uncomment the next line if you don't want to decrypt SSL traffic.
+            // oFCSF = (oFCSF & ~FiddlerCoreStartupFlags.DecryptSSL);
+            //
+            // NOTE: Unless you disable the option to decrypt HTTPS traffic, makecert.exe
+            // must be present in this executable's folder.
+
+            // NOTE: In the next line, you can pass 0 for the port (instead of 8877) to have FiddlerCore auto-select an available port
+            Fiddler.FiddlerApplication.Startup(8877, oFCSF);
+
+            FiddlerApplication.Log.LogFormat("Starting with settings: [{0}]", oFCSF);
+            FiddlerApplication.Log.LogFormat("Using Gateway: {0}", (CONFIG.bForwardToGateway) ? "TRUE" : "FALSE");
+
+            Console.WriteLine("Hit CTRL+C to end session.");
+
+            // We'll also create a HTTPS listener, useful for when FiddlerCore is masquerading as a HTTPS server
+            // instead of acting as a normal CERN-style proxy server.
+            _oSecureEndpoint = FiddlerApplication.CreateProxyEndpoint(iSecureEndpointPort, true, sSecureEndpointHostname);
+            if (null != _oSecureEndpoint)
+            {
+                FiddlerApplication.Log.LogFormat("Created secure end point listening on port {0}, using a HTTPS certificate for '{1}'", iSecureEndpointPort, sSecureEndpointHostname);
+            }
+
             Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
             AskUsersInput();
 
@@ -60,7 +107,7 @@ namespace FiddlerTestRunnerConsole
         {
             _needMoreInput = false;
             Log.Info("DoQuit Called...");
-            //if (null != oSecureEndpoint) oSecureEndpoint.Dispose();
+            if (null != _oSecureEndpoint) _oSecureEndpoint.Dispose();
             Fiddler.FiddlerApplication.Shutdown();
             Thread.Sleep(500);
             Log.Info("Quit Completed.");
@@ -141,7 +188,7 @@ namespace FiddlerTestRunnerConsole
                         break;
 
                 }
-                if (inputCount++ > inputCleanupThreshold)
+                if (++inputCount > inputCleanupThreshold)
                 {
                     Log.Info(m => m("input count '{0}' > InputCleanupThreshold '{1}'", inputCount, inputCleanupThreshold));
                     GarbageCollection();
