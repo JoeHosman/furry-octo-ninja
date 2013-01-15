15d3abe068d774cbaa50a699e869166217849e8f
JoeHosman <joe.hosman@gmail.com>
2013-01-14 19:00:37 -0600

    started mvc website - with bootstrap theme

---
+++ src/Star.FiddlerRunner.AdminWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Star.FiddlerRunner.AdminWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
