4f03442f2972a53b35454ac3a6fba582ea8ef5c2
JoeHosman <joe.hosman@gmail.com>
2013-03-01 13:01:49 -0600

    added controls
    toggle proxy control
    proxy listening port setting control

---
+++ src/FiddlerRunnerGUI/Controls/ProxyListeningPortSettingControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FiddlerRunnerGUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for ProxyListeningPortSettingControl.xaml
+    /// </summary>
+    public partial class ProxyListeningPortSettingControl : UserControl
+    {
+        public ProxyListeningPortSettingControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
