bc1001e3482a09be077b9183034441cf05607282
JoeHosman <joe.hosman@gmail.com>
2013-03-04 16:18:01 -0600

    created service

---
+++ src/FiddlerRunnerProxy.Service/FiddlerRunnerProxyWindowsService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiddlerRunnerProxy.Service
+{
+    public partial class FiddlerRunnerProxyWindowsService : ServiceBase
+    {
+        public FiddlerRunnerProxyWindowsService()
+        {
+            InitializeComponent();
+
+            SetupWindowsServiceLog();
+        }
+
+        private void SetupWindowsServiceLog()
+        {
+            string sourceName = "MySource";
+            string logName = "MyNewLog";
+            if (!EventLog.SourceExists(sourceName))
+            {
+
+                EventLog.CreateEventSource(sourceName, logName);
+            }
+
+            eventLog1.Source = sourceName;
+            eventLog1.Log = logName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue.");
+        }  
+    }
+}
